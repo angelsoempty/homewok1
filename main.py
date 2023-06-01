2ae2dd7d0640af2ab8b8b46d448f534b9ad74bb8
angelsoempty <denatamanov1@gmail.com>
2023-06-01 16:01:03 +0300

    #2

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
-class PrimeGenerator:
-    def __init__(self):
-        self.next_prime = 2
-    def is_prime(self, num):
-        if num < 2:
-            return False
-        for i in range(2, int(num ** 0.5) + 1):
-            if num % i == 0:
-                return False
-        return True
-    def generate(self):
-        while True:
-            if self.is_prime(self.next_prime):
-                prime = self.next_prime
-                self.next_prime += 1
-                return prime
-            self.next_prime += 1
+def average_closure():
+    numbers = []
+    total = 0
+    def add_number(number):
+        nonlocal total
+        numbers.append(number)
+        total += number
+    def get_average():
+        if not numbers:
+            return None
+        return total / len(numbers)
+    return add_number, get_average
 
-prime_generator = PrimeGenerator()
-print(prime_generator.generate())
-print(prime_generator.generate())
-print(prime_generator.generate())
+add_number, get_average = average_closure()
+add_number(5)
+add_number(10)
+add_number(15)
+print(get_average())
