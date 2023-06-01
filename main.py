0db8b43fdf1af4f1c4611cc18f13ea68e120b847
angelsoempty <denatamanov1@gmail.com>
2023-06-01 16:02:21 +0300

    #3

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
-def average_closure():
-    numbers = []
-    total = 0
-    def add_number(number):
-        nonlocal total
-        numbers.append(number)
-        total += number
-    def get_average():
-        if not numbers:
-            return None
-        return total / len(numbers)
-    return add_number, get_average
+import random
+class PasswordGenerator:
+    def __init__(self, length, characters):
+        self.length = length
+        self.characters = characters
+    def generate_password(self):
+        password = ''.join(random.choice(self.characters) for _ in range(self.length))
+        return password
+characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()"
 
-add_number, get_average = average_closure()
-add_number(5)
-add_number(10)
-add_number(15)
-print(get_average())
+password_generator = PasswordGenerator(8, characters)
+print(password_generator.generate_password())
+print(password_generator.generate_password())
